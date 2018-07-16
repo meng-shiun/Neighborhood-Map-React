050ede4ba111a6d8f61c2f9adf0015d317c06b87
meng-Shiun <lin.mengshiun@gmail.com>
2018-07-16 17:32:39 +0200

    Add a filterable List of Locations

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
 import Map from './components/Map'
-import SearchBar from './components/SearchBar'
 import ListLocation from './components/ListLocation'
 
 import './App.css'
+
+const defaultLocations = [
+  {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
+  {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
+  {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
+  {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
+  {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
+  {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+];
 
 class App extends Component {
   render() {
@@ -13,12 +21,13 @@ class App extends Component {
           <h1 className="App-title">Neighborhood Map</h1>
         </header>
 
-        <SearchBar/>
-        <ListLocation/>
+        <ListLocation
+          locations={defaultLocations}
+          />
+
         <Map/>
       </div>
     );
   }
 }
-
 export default App;
--- src/components/ListLocation.js
+++ src/components/ListLocation.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
+import './ListLocation.css'
 
-const ListLocation = (props) => {
-  return (
-    <div>ListLocation</div>
-  )
+
+class ListLocation extends Component {
+  state = {
+    filter: '',
+    getLocations: this.props.locations
+  }
+
+  handleChange = (e) => {
+    const regexp = new RegExp(e.target.value, 'i')
+
+    this.setState({filter: e.target.value})
+    this.setState((state, props) => ({
+      getLocations: props.locations.filter(el => regexp.test(el.title))
+    }))
+  }
+
+  handleListClick = (loc) => {
+    console.log('select:', loc);
+  }
+
+  render() {
+    return (
+      <div className='list-location'>
+        <input
+          type='text'
+          value={this.state.filter}
+          placeholder='search location'
+          onChange={this.handleChange}
+          />
+
+        <p>Filter: {this.state.filter}</p>
+
+        <ul>
+          {this.state.getLocations.map(loc => (
+            <li
+              key={loc.title}
+              onClick={() => this.handleListClick(loc)}>
+              {loc.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default ListLocation
